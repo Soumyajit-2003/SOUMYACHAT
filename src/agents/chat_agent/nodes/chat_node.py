9a7bbbe732dce99c408c72a1e831c1f885e90298
SUBHRAJYOTI MANDAL <subhrajyotimandal1216@gmail.com>
2026-01-20 12:55:43 +0530

    added langsmith tracking

--- src/agents/chat_agent/nodes/chat_node.py
+++ src/agents/chat_agent/nodes/chat_node.py
@@ -1,4 +1,6 @@
+from click import prompt
 from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 import os
 from src.agents.chat_agent.states.chat_agent_state import ChatAgentState
@@ -6,12 +8,17 @@ from src.agents.chat_agent.tools.date_time import get_current_datetime
 from src.agents.chat_agent.tools.web_search_tool import search_the_web
 
 
+template = """You are a helpful AI assistant. give detailed and proper structured answers to user queries 
+Message History: {message_history}"""
+
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-
+os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
+os.environ["LANGSMITH_TRACING"] = "true"
 
 def chat(state: ChatAgentState) -> ChatAgentState:
-
+    """"""
+    prompt_template = ChatPromptTemplate.from_template(template = template)
     model = ChatGroq(
         model="openai/gpt-oss-120b",
         api_key=GROQ_API_KEY,
@@ -21,5 +28,6 @@ def chat(state: ChatAgentState) -> ChatAgentState:
             get_current_datetime, search_the_web
         ]
     )
-    answer = model.invoke(state["message"])
+    chain = prompt_template | model
+    answer = chain.invoke(state["message"])
     return {"message": [answer]}
